5f2e4b79a15885eebf478733ae5063d504843ff5
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-04-10 14:15:15 +0430

    creation of search bar component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div><SearchBar @termChange="onTermChange" /></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import SearchBar from './components/SearchBar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    SearchBar,
+  },
+  methods: {
+    onTermChange(value) {
+      console.log(value);
+    },
   },
 };
 </script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input @input="onInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  methods: {
+    onInput(event) {
+      // emit the function form here to parent
+      // to passing value from here to parent state by callback event system
+
+      this.$emit('termChange', event.target.value);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
